2e4be07398ed06caf6623c3b920600989f162e50
rfaguiar <rfaguiar1@gmail.com>
2017-01-17 16:55:43 -0200

    finalizado mapeamento jpa de pedido.

--- PedidoVenda/src/main/java/com/rfaguiar/pedidovenda/model/ItemPedido.java
+++ PedidoVenda/src/main/java/com/rfaguiar/pedidovenda/model/ItemPedido.java
@@ -3,6 +3,17 @@ package com.rfaguiar.pedidovenda.model;
 import java.io.Serializable;
 import java.math.BigDecimal;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name ="item_pedido")
 public class ItemPedido implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -12,31 +23,43 @@ public class ItemPedido implements Serializable {
 	private BigDecimal valorUnitario;
 	private Produto produto;
 	private Pedido pedido;
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	public Long getId() {
 		return id;
 	}
 	public void setId(Long id) {
 		this.id = id;
 	}
+	
+	@Column(nullable = false, length = 3)
 	public Integer getQuantidade() {
 		return quantidade;
 	}
 	public void setQuantidade(Integer quantidade) {
 		this.quantidade = quantidade;
 	}
+	
+	@Column(name = "valor_unitario", nullable = false, precision = 10, scale = 2)
 	public BigDecimal getValorUnitario() {
 		return valorUnitario;
 	}
 	public void setValorUnitario(BigDecimal valorUnitario) {
 		this.valorUnitario = valorUnitario;
 	}
+	
+	@ManyToOne
+	@JoinColumn(name = "produto_id", nullable = false)
 	public Produto getProduto() {
 		return produto;
 	}
 	public void setProduto(Produto produto) {
 		this.produto = produto;
 	}
+	
+	@ManyToOne
+	@JoinColumn(name = "pedido_id", nullable = false)
 	public Pedido getPedido() {
 		return pedido;
 	}
